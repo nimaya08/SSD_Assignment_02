6285207115e87a091a618592074281a7f4957ee9
lakmina98 <it18184686@my.sliit.lk>
2021-10-03 23:58:53 +0530

    api for fetching google contacts

---
+++ backend/config.ts
@@ -0,0 +1,4 @@
+export const CLIENT_ID = process.env.CLIENT_ID || "108962119082-o2e59gjot3o3ukbtpqfmur7krhh00f3r.apps.googleusercontent.com"
+export const CLIENT_SECRET = process.env.CLIENT_SECRET || "_lYMicunPcbigPzEKc2mhE7d"
+export const JWT_SECRET = "shh"
+export const COOKIE_NAME = "auth_token"
