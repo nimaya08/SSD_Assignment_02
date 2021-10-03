05d2f6baef62b7fd73507f7fe72d112139ef0590
Sandeep Fernando <it18047066@my.sliit.lk>
2021-10-04 00:16:53 +0530

    ui fixes done

--- backend/routes/api/googlesignin.js
+++ backend/routes/api/googlesignin.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { OAuth2Client } = require('google-auth-library')
+// const key = process.env.REACT_APP_GOOGLE_CLIENT_ID
+const key = "181267109282-67mtbok008de2sbpgt9dpotdjkik5usp.apps.googleusercontent.com"
+const client = new OAuth2Client(key)
 
-const googlesignin = require("../../middleware/auth")
+const verifyUser = (req, res, next) => {
+    client.verifyIdToken({ idToken: req.body.tokenId, audience: key })
+        .then(response => {
+            res.status(200).send(response.getPayload());
+        }).catch(reason => {
+            console.log(reason);
+            res.status(403).send(reason);
+        })
+}
 
-// router.post('/', googlesignin);
-
-router.post('/', (req,res) => {
-    res.send('GameNEWS_news')
-})
+router.post('/', function (req, res) {
+    verifyUser(req, res, function ({ email_verified, email, name }) {
+        res.status(200).send({ email_verified, email, name });
+    });
+});
 
 module.exports = router;
